32f3859ec25b620ad8925bef751f8b3756498fa8
vuminhcute2k2 <Minhvu69696969@gmail.com>
2024-05-15 11:42:32 +0700

    Initial Commit

---
+++ Message_chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Message_chat
+//
+//  Created by Minh Vũ on 15/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
